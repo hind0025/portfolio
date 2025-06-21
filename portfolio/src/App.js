5d71f07a44701c47f1b51312e40de111a3962c30
hind0025 <s.hindveer2004@gmail.com>
2025-06-21 20:26:41 +0530

    landingpage

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -7,6 +7,14 @@ function App() {
   return (
     <>
     <Navbar/>
+    <div className="home-container">
+      <h1 className="name-heading">Hindveer Singh</h1>
+      <p className="intro-paragraph">
+        I'm a passionate problem solver and web developer who loves turning ideas into  functional websites.
+        I never give up—challenges just make the process more exciting!
+      </p>
+    </div>
+    
 
     </>
   );
