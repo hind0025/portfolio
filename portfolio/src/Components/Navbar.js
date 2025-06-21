e504fa32740f03e4e8fb83b96a74faf0fe1640e2
hind0025 <s.hindveer2004@gmail.com>
2025-06-21 19:49:09 +0530

    navbar

---
+++ portfolio/src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../Styles/Navbar.css'; 
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-logo">My Portfolio</div>
+      <ul className="nav-links">
+        <li><a href="#about">About</a></li>
+        <li><a href="#techstack">Tech Stack</a></li>
+        <li><a href="#projects">Projects</a></li>
+      </ul>
+    </nav>
+  );
+}
+export default Navbar;
